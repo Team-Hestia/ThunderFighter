f39c714975c967268064a65389be08687fbff2f3
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2016-06-19 19:07:35 +0300

    Changed Player appearance Random enemy appearance created

--- ThunderFighter/Fighter.cs
+++ ThunderFighter/Fighter.cs
@@ -10,23 +10,34 @@
         {
             List<Pixel> body = new List<Pixel>();
 
-            body.Add(new Pixel(0, 0, '=', ConsoleColor.Black));
+            body.Add(new Pixel(1, 0, '/', ConsoleColor.Black));  // wing 1
+            body.Add(new Pixel(2, 0, '\\', ConsoleColor.Black));
 
-            body.Add(new Pixel(-1, 0, '>', ConsoleColor.Red));
+            body.Add(new Pixel(1, 1, '\\', ConsoleColor.Black));
+            body.Add(new Pixel(2, 1, '*', ConsoleColor.Red));
+            body.Add(new Pixel(3, 1, '\\', ConsoleColor.Black));
 
-            body.Add(new Pixel(0, -2, '-', ConsoleColor.Black));
-            body.Add(new Pixel(0, -1, '\\', ConsoleColor.Black));
-            body.Add(new Pixel(0, 1, '/', ConsoleColor.Black));
-            body.Add(new Pixel(0, 2, '-', ConsoleColor.Black));
+            body.Add(new Pixel(1, 2, '/', ConsoleColor.Black));
+            body.Add(new Pixel(4, 2, '\\', ConsoleColor.Black));  // wing 1
 
-            body.Add(new Pixel(1, -1, '\\', ConsoleColor.Black));
-            body.Add(new Pixel(1, 0, '=', ConsoleColor.Black));
-            body.Add(new Pixel(1, 1, '/', ConsoleColor.Black));
+            body.Add(new Pixel(0, 3, ']', ConsoleColor.Red));   // body
+            body.Add(new Pixel(1, 3, '=', ConsoleColor.Black));
+            body.Add(new Pixel(2, 3, '=', ConsoleColor.Black));
+            body.Add(new Pixel(3, 3, '=', ConsoleColor.Black));
+            body.Add(new Pixel(4, 3, '=', ConsoleColor.Black)); 
+            body.Add(new Pixel(5, 3, 'D', ConsoleColor.Black)); 
+            body.Add(new Pixel(6, 3, '>', ConsoleColor.Black)); // body
 
-            body.Add(new Pixel(2, 0, '=', ConsoleColor.Black));
+            body.Add(new Pixel(1, 6, '\\', ConsoleColor.Black));  // wing 2
+            body.Add(new Pixel(2, 6, '/', ConsoleColor.Black));
 
-            body.Add(new Pixel(3, 0, '=', ConsoleColor.DarkCyan));
-            body.Add(new Pixel(4, 0, '>', ConsoleColor.DarkCyan));
+            body.Add(new Pixel(1, 5, '/', ConsoleColor.Black));
+            body.Add(new Pixel(2, 5, '*', ConsoleColor.Red));
+            body.Add(new Pixel(3, 5, '/', ConsoleColor.Black));
+
+            body.Add(new Pixel(1, 4, '\\', ConsoleColor.Black));
+            body.Add(new Pixel(4, 4, '/', ConsoleColor.Black)); // wing 2
+
 
             return body;
         }
@@ -69,7 +80,7 @@
                 else if (userInput.Key == ConsoleKey.Spacebar) // TODO handle with EventHandler(OnKeyPress)
                 {
                     List<Pixel> bulletBody = new List<Pixel>();
-                    bulletBody.Add(new Pixel(5, 0, '-', ConsoleColor.Black));
+                    bulletBody.Add(new Pixel(7, 3, '-', ConsoleColor.Red));
 
                     Engine.bullets.Add(new Bullet(this.Field, new Point2D(this.Position), bulletBody));
                 }
--- ThunderFighter/KillerWingEnemy.cs
+++ ThunderFighter/KillerWingEnemy.cs
@@ -8,22 +8,53 @@
         private static List<Pixel> EnemyBody(Point2D pos)
         {
             List<Pixel> body = new List<Pixel>();
+            var randomEnemy = RandomProvider.Instance;
+            int taleLength = randomEnemy.Next(2, 15);
 
-            body.Add(new Pixel(0, 0, '<', ConsoleColor.Blue));
-            body.Add(new Pixel(1, -1, '=', ConsoleColor.Blue));
-            body.Add(new Pixel(1, 1, '=', ConsoleColor.Blue));
-            body.Add(new Pixel(2, 0, ':', ConsoleColor.Blue));
-            body.Add(new Pixel(2, -2, '/', ConsoleColor.DarkBlue));
-            body.Add(new Pixel(2, 2, '\\', ConsoleColor.DarkBlue));
-
-            body.Add(new Pixel(3, 0, '=', ConsoleColor.Blue));
-            body.Add(new Pixel(4, 0, '=', ConsoleColor.Blue));
-            body.Add(new Pixel(5, 0, '=', ConsoleColor.Blue));
-            body.Add(new Pixel(6, 0, '=', ConsoleColor.Blue));
-            body.Add(new Pixel(7, 0, '=', ConsoleColor.Blue));
-            body.Add(new Pixel(8, 0, '=', ConsoleColor.Blue));
-            body.Add(new Pixel(9, 0, '=', ConsoleColor.Blue));
-            body.Add(new Pixel(10, 0, '=', ConsoleColor.Blue));
+            switch (randomEnemy.Next(0, 4))
+            {
+                case 0:
+                    body.Add(new Pixel(0, 0, '<', ConsoleColor.DarkMagenta));
+                    body.Add(new Pixel(1, -1, '(', ConsoleColor.DarkMagenta));
+                    body.Add(new Pixel(1, 1, '(', ConsoleColor.DarkMagenta));
+                    body.Add(new Pixel(2, 0, '<', ConsoleColor.DarkMagenta));
+                    for (int i = 0; i < taleLength; i++)
+                    {
+                        body.Add(new Pixel(i + 3, 0, '-', ConsoleColor.DarkMagenta));
+                    }
+                    break;
+                case 1:
+                    body.Add(new Pixel(0, 0, '{', ConsoleColor.DarkCyan));
+                    body.Add(new Pixel(1, -1, '\\', ConsoleColor.DarkCyan));
+                    body.Add(new Pixel(1, 1, '/', ConsoleColor.DarkCyan));
+                    body.Add(new Pixel(2, 0, 'B', ConsoleColor.DarkCyan));
+                    for (int i = 0; i < taleLength; i++)
+                    {
+                        body.Add(new Pixel(i + 3, 0, '-', ConsoleColor.DarkCyan));
+                    }
+                    break;
+                case 2:
+                    body.Add(new Pixel(0, 0, '[', ConsoleColor.DarkGreen));
+                    body.Add(new Pixel(1, -1, 'O', ConsoleColor.DarkGreen));
+                    body.Add(new Pixel(1, 1, 'O', ConsoleColor.DarkGreen));
+                    body.Add(new Pixel(2, 0, 'O', ConsoleColor.DarkGreen));
+                    for (int i = 0; i < taleLength; i++)
+                    {
+                        body.Add(new Pixel(i + 3, 0, '-', ConsoleColor.DarkGreen));
+                    }
+                    break;
+                default:
+                    body.Add(new Pixel(0, 0, '<', ConsoleColor.Blue));
+                    body.Add(new Pixel(1, -1, '/', ConsoleColor.Blue));
+                    body.Add(new Pixel(1, 1, '\\', ConsoleColor.Blue));
+                    body.Add(new Pixel(2, 0, ':', ConsoleColor.Blue));
+                    for (int i = 0; i < taleLength; i++)
+                    {
+                        body.Add(new Pixel(i + 3, 0, '-', ConsoleColor.Blue));
+                    }
+                    break;
+            }
+            
             return body;
         }
         public KillerWingEnemy(Field field, Point2D position) : this(field, position, EnemyBody(position))
