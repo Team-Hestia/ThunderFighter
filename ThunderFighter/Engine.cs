c05188675a56a1b3077ecc73dbffb2b5d3839673
madmarks <bratmiro@gmail.com>
2016-06-23 21:04:33 +0300

    animation when entity destroyed, collision detection improved

--- ThunderFighter/Engine.cs
+++ ThunderFighter/Engine.cs
@@ -7,35 +7,38 @@
     using System.Threading;
     using System.Threading.Tasks;
 
-    class Engine
+    internal class Engine
     {
-        public GameStatus GameStatus { get; set; }
-
         private MessageBox welcomeMessageBox;
         private MessageBox gameOverMessageBox;
         private MessageBox pauseMessageBox;
-
-        private static Random rand = new Random();
 
         private Field field;
         private Fighter player;
         private GameLevel gameLevel;
 
+        private Type[] playerClassTypes;
         private Type[] enemyClassTypes;
+        private Type[] buildingClassTypes;
 
-        /* Enemies info */
-        private static List<Enemy> enemies = new List<Enemy>();
-
-        /* Bullets info */
-        public static List<Bullet> bullets = new List<Bullet>();
+        private List<Fighter> players;
+        private List<Enemy> enemies;
+        private List<Building> buildings;
 
         public Engine(Field field, Fighter player, GameLevel gameLevel)
         {
             this.Field = field;
             this.Player = player;
             this.GameLevel = gameLevel;
+            this.GameStatus = GameStatus.Welcome;
 
+            this.PlayerClassTypes = ReflectiveArray.GetTypeOfDerivedClasses<Fighter>();
             this.EnemyClassTypes = ReflectiveArray.GetTypeOfDerivedClasses<Enemy>();
+            this.BuildingClassTypes = ReflectiveArray.GetTypeOfDerivedClasses<Building>();
+
+            this.players = new List<Fighter>();
+            this.enemies = new List<Enemy>();
+            this.buildings = new List<Building>();
 
             this.welcomeMessageBox = new MessageBox(
                 this.field,
@@ -54,6 +57,86 @@
                 "GAME OVER!\nPress any key to continue...",
                 MessageBoxDrawing.DrawCentered,
                 MessageBoxTextAlignment.Center);
+        }
+
+        public GameStatus GameStatus { get; private set; }
+
+        public Field Field
+        {
+            get
+            {
+                return this.field;
+            }
+
+            private set
+            {
+                this.field = value;
+            }
+        }
+
+        public Fighter Player
+        {
+            get
+            {
+                return this.player;
+            }
+
+            private set
+            {
+                this.player = value;
+            }
+        }
+
+        public GameLevel GameLevel
+        {
+            get
+            {
+                return this.gameLevel;
+            }
+
+            private set
+            {
+                this.gameLevel = value;
+            }
+        }
+
+        public Type[] PlayerClassTypes
+        {
+            get
+            {
+                return this.playerClassTypes;
+            }
+
+            private set
+            {
+                this.playerClassTypes = value;
+            }
+        }
+
+        public Type[] EnemyClassTypes
+        {
+            get
+            {
+                return this.enemyClassTypes;
+            }
+
+            private set
+            {
+                this.enemyClassTypes = value;
+            }
+        }
+
+        public Type[] BuildingClassTypes
+        {
+            get
+            {
+                return this.buildingClassTypes;
+            }
+
+            private set
+            {
+                this.buildingClassTypes = value;
+            }
         }
 
         public void Start()
@@ -85,19 +168,18 @@
                         break;
                 }
 
-                Thread.Sleep(90);
+                Thread.Sleep(100);
             }
         }
 
         private void Welcome()
         {
-            this.player.Clear();
-            this.EnemiesClear();
-            this.BulletsClear();
+            this.Clear();
 
-            this.player = new Fighter(this.Field, new Point2D(10, 5));
-            enemies.Clear();
-            bullets.Clear();
+            this.enemies.Clear();
+            this.buildings.Clear();
+
+            this.Player = new Fighters.ThunderFighterOne(this.Field, new Point2D(10, 5), EntityState.Strong);
 
             this.welcomeMessageBox.Draw();
             this.HandleWelcomeKeys();
@@ -124,37 +206,56 @@
 
         private void Play()
         {
-            // Clear:
-            this.player.Clear();
-            this.EnemiesClear();
-            this.BulletsClear();
-            // clear bombs
-            // clear missiles
-
-            // Update:
-            // TODO: The code for keyboard handling should not be in this method. There should be a central KeyboardHandler class.
-            this.player.Move(); 
-            this.EnemiesMove();
-            this.BulletsMove();
-            // update bombs
-            // update missiles
-
-            // Collision Detection:
-            this.DetectEnemyBulletCollisions();
-            this.DetectPlayerEnemyCollision();
-            // check Enemies-Missiles collisions
-            // check Enemies-Player collisions
-            // check Bombs-Buildings collisions
-
-            // Draw:
-            this.player.Draw();
-            this.EnemiesDraw();
-            this.BulletsDraw();
-            // draw bombs
-            // draw missiles
+            this.Clear();
+            this.Move();
+            this.CollisionDetection();
+            this.Draw();
         }
 
-        // TODO:
+        private void Clear()
+        {
+            this.Player.Clear();
+            this.EnemiesClear();
+            // TODO: this.BuildingsClear();
+            this.BulletsClear();
+            // TODO: this.BombsClear();
+            // TODO: this.MissilesClear();
+        }
+
+        private void Move()
+        {
+            // TODO: The code for keyboard handling should not be in this method. There should be a central KeyboardHandler class.
+            this.Player.Move();
+            this.EnemiesMove();
+            // TODO: this.BuildingsMove();
+            this.BulletsMove();
+            // TODO: this.BombsMove();
+            // TODO: this.MissilesMove();
+        }
+
+        private void CollisionDetection()
+        {
+            // TODO: this.DetectPlayerBulletCollisions();
+            this.DetectPlayerEnemyCollisions();
+
+            this.DetectEnemyBulletCollisions();
+            // TODO: this.DetectEnemyMissileCollisions();
+
+            // TODO: this.DetectBombBuildingCollisions();
+        }
+
+        private void Draw()
+        {
+            // Draw:
+            this.PlayerDraw();
+            this.EnemiesDraw();
+            // TODO: this.BuildingsDraw();
+            this.BulletsDraw();
+            // TODO: this.BombsDraw();
+            // TODO: this.MissilesDraw();
+        }
+
+        // TODO: handle pausing
         //private void HandlePausing()
         //{
         //    if (Console.KeyAvailable)
@@ -204,26 +305,25 @@
 
         private void DetectEnemyBulletCollisions()
         {
-            for (int i = 0; i < Engine.enemies.Count; i++)
+            for (int i = 0; i < this.enemies.Count; i++)
             {
-                for (int j = 0; j < Engine.bullets.Count; j++)
+                for (int j = 0; j < this.Player.Bullets.Count; j++)
                 {
-                    if (Engine.enemies[i].Body
-                        .Exists(enemyPixel => Engine.bullets[j].Body.Exists(bulletPixel =>
-                            bulletPixel.Coordinate == enemyPixel.Coordinate ||
-                            (bulletPixel.Coordinate.Y == enemyPixel.Coordinate.Y &&
-                             (bulletPixel.Coordinate.X - 1) == enemyPixel.Coordinate.X) ||
-                            (bulletPixel.Coordinate.Y == enemyPixel.Coordinate.Y &&
-                             (bulletPixel.Coordinate.X - 2) == enemyPixel.Coordinate.X))))
+                    if (this.enemies[i].State == (int)EntityState.Strong &&
+                        this.enemies[i].State == (int)EntityState.Strong &&
+                        this.enemies[i].Body
+                            .Exists(enemyPixel => this.Player.Bullets[j].Body.Exists(bulletPixel =>
+                                enemyPixel.Coordinate.Y == bulletPixel.Coordinate.Y &&
+                                0 <= (bulletPixel.Coordinate.X - enemyPixel.Coordinate.X) &&
+                                (bulletPixel.Coordinate.X - enemyPixel.Coordinate.X) <= this.Player.Bullets[j].DeltaX)))
                     {
-                        Engine.enemies[i].IsDestroyed = true;
-                        Engine.bullets[j].IsDestroyed = true;
+                        this.enemies[i].State = (int)EntityState.HalfDestroyed;
+                        this.enemies[i].DeltaX = 0;
+                        this.enemies[i].DeltaY = 0;
 
-                        // TODO: refactor this (we need to draw crashed enemy)
-                        Engine.enemies.RemoveAt(i);
-                        i--;
-                        Engine.bullets.RemoveAt(j);
-                        j--;
+                        this.Player.Bullets[j].State = (int)EntityState.HalfDestroyed;
+                        this.Player.Bullets[j].DeltaX = 0;
+                        this.Player.Bullets[j].DeltaY = 0;
 
                         break;
                     }
@@ -231,48 +331,62 @@
             }
         }
 
-        private void DetectPlayerEnemyCollision()
+        private void DetectPlayerEnemyCollisions()
         {
-            for (int i = 0; i < Engine.enemies.Count; i++)
+            for (int i = 0; i < this.enemies.Count; i++)
             {
-                if (Engine.enemies[i].Body
+                if (this.Player.State == (int)EntityState.Strong &&
+                    this.enemies[i].State == (int)EntityState.Strong &&
+                    this.enemies[i].Body
                         .Exists(enemyPixel => this.Player.Body.Exists(playerPixel =>
-                            playerPixel.Coordinate == enemyPixel.Coordinate ||
-                            (playerPixel.Coordinate.Y == enemyPixel.Coordinate.Y &&
-                             (playerPixel.Coordinate.X - 1) == enemyPixel.Coordinate.X) ||
-                            (playerPixel.Coordinate.Y == enemyPixel.Coordinate.Y &&
-                             (playerPixel.Coordinate.X - 2) == enemyPixel.Coordinate.X))))
+                                enemyPixel.Coordinate.Y == playerPixel.Coordinate.Y &&
+                                0 <= (playerPixel.Coordinate.X - enemyPixel.Coordinate.X) &&
+                                (playerPixel.Coordinate.X - enemyPixel.Coordinate.X) <= Math.Abs(this.enemies[i].DeltaX))))
                 {
-                    Engine.enemies[i].IsDestroyed = true;
+                    this.Player.State = (int)EntityState.HalfDestroyed;
 
-                    // TODO: refactor this (we need to draw crashed enemy)
-                    Engine.enemies.RemoveAt(i);
-                    i--;
-
-                    this.Player.IsDestroyed = true;
-                    this.GameStatus = GameStatus.GameOver;
+                    this.enemies[i].State = (int)EntityState.HalfDestroyed;
+                    this.enemies[i].DeltaX = 0;
+                    this.enemies[i].DeltaY = 0;
 
                     break;
                 }
             }
         }
 
+        private void PlayerDraw()
+        {
+            this.Player.Draw();
+
+            if (this.Player.IsDestroyed)
+            {
+                this.GameStatus = GameStatus.GameOver;
+            }
+        }
+
         private void EnemiesClear()
         {
-            foreach (var enemy in Engine.enemies)
+            for (int i = 0; i < this.enemies.Count; i++)
             {
-                enemy.Clear();
+                this.enemies[i].Clear();
+
+                if (this.enemies[i].IsDestroyed)
+                {
+                    this.enemies.RemoveAt(i);
+                    i--;
+                }
             }
         }
 
         private void EnemiesMove()
         {
-            for (int i = 0; i < Engine.enemies.Count; i++)
+            for (int i = 0; i < this.enemies.Count; i++)
             {
-                Engine.enemies[i].Move();
-                if (Engine.enemies[i].Body.All(pixel => pixel.Coordinate.X < 0))
+                this.enemies[i].Move();
+
+                if (this.enemies[i].Body.All(pixel => pixel.Coordinate.X < 0))
                 {
-                    Engine.enemies.RemoveAt(i);
+                    this.enemies.RemoveAt(i);
                     i--;
                 }
             }
@@ -282,59 +396,64 @@
 
         private void EnemiesDraw()
         {
-            foreach (var enemy in Engine.enemies)
+            for (int i = 0; i < this.enemies.Count; i++)
             {
-                if (!enemy.IsDestroyed) // TODO: Draw() must be refactored a lot
-                {
-                    enemy.Draw();
-                }
+                this.enemies[i].Draw();
             }
         }
 
         private void SpawnNewEnemy()
         {
-            int indexOfRandomEnemyClass = rand.Next(0, this.EnemyClassTypes.Count());
+            int indexOfRandomEnemyClass = RandomProvider.Instance.Next(0, this.EnemyClassTypes.Count());
 
-            while (Engine.enemies.Count < 7) //TODO: use gameLevel
+            // TODO: use gameLevel instead of 7
+            while (this.enemies.Count < 7)
             {
                 // TODO: use enemy width and height
-                int x = rand.Next(this.Field.Width, 2 * this.Field.Width);
-                int y = rand.Next(2, this.Field.Height - 3);
+                int x = RandomProvider.Instance.Next(this.Field.Width, 2 * this.Field.Width);
+                int y = RandomProvider.Instance.Next(2, this.Field.Height - 3);
 
-                var randomEnemy = (Enemy) Activator.CreateInstance(
+                var randomEnemy = (Enemy)Activator.CreateInstance(
                     this.EnemyClassTypes[indexOfRandomEnemyClass],
                     this.Field,
-                    new Point2D(x, y));
+                    new Point2D(x, y),
+                    EntityState.Strong);
 
-                if (Engine.enemies.Exists(enemy => enemy.Body.Exists(pixel => randomEnemy.Body.Exists(newEnemyPixel => newEnemyPixel.Coordinate == pixel.Coordinate))))
+                if (this.enemies.Exists(enemy => enemy.Body.Exists(pixel => randomEnemy.Body.Exists(newEnemyPixel => newEnemyPixel.Coordinate == pixel.Coordinate))))
                 {
                     continue;
                 }
                 else
                 {
-                    enemies.Add(randomEnemy);
-                    indexOfRandomEnemyClass = rand.Next(0, this.EnemyClassTypes.Count());
-                    break;
+                    this.enemies.Add(randomEnemy);
+                    indexOfRandomEnemyClass = RandomProvider.Instance.Next(0, this.EnemyClassTypes.Count());
                 }
             }
         }
 
         private void BulletsClear()
         {
-            foreach (var bullet in Engine.bullets)
+            for (int i = 0; i < this.Player.Bullets.Count; i++)
             {
-                bullet.Clear();
+                this.Player.Bullets[i].Clear();
+
+                if (this.Player.Bullets[i].IsDestroyed)
+                {
+                    this.Player.Bullets.RemoveAt(i);
+                    i--;
+                }
             }
         }
 
         private void BulletsMove()
         {
-            for (int i = 0; i < Engine.bullets.Count; i++)
+            for (int i = 0; i < this.Player.Bullets.Count; i++)
             {
-                Engine.bullets[i].Move();
-                if (Engine.bullets[i].Body.All(pixel => pixel.Coordinate.X > this.Field.Width - 1))
+                this.Player.Bullets[i].Move();
+
+                if (this.Player.Bullets[i].Body.All(pixel => pixel.Coordinate.X > this.Field.Width - 1))
                 {
-                    Engine.bullets.RemoveAt(i);
+                    this.Player.Bullets.RemoveAt(i);
                     i--;
                 }
             }
@@ -342,64 +461,9 @@
 
         private void BulletsDraw()
         {
-            foreach (var bullet in Engine.bullets)
+            for (int i = 0; i < this.Player.Bullets.Count; i++)
             {
-                if (!bullet.IsDestroyed) // TODO: Draw() must be refactored a lot
-                {
-                    bullet.Draw();
-                }
-            }
-        }
-
-        public Field Field
-        {
-            get
-            {
-                return this.field;
-            }
-
-            private set
-            {
-                this.field = value;
-            }
-        }
-
-        public Fighter Player
-        {
-            get
-            {
-                return this.player;
-            }
-
-            private set
-            {
-                this.player = value;
-            }
-        }
-
-        public GameLevel GameLevel
-        {
-            get
-            {
-                return this.gameLevel;
-            }
-
-            private set
-            {
-                this.gameLevel = value;
-            }
-        }
-
-        public Type[] EnemyClassTypes
-        {
-            get
-            {
-                return this.enemyClassTypes;
-            }
-
-            private set
-            {
-                this.enemyClassTypes = value;
+                this.Player.Bullets[i].Draw();
             }
         }
     }
