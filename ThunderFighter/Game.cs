d1ef4590b3fddbc4b894943b28be7f9f96778bb2
madmarks <bratmiro@gmail.com>
2016-07-07 09:53:03 +0300

    remove unnecessary pieces of code

--- ThunderFighter/Game.cs
+++ ThunderFighter/Game.cs
@@ -4,13 +4,11 @@
     {
         public static void Start()
         {
-            var theme = new Theme(ThemeColor.black);
+            var theme = new Theme(ThemeColor.Black);
             var field = new Field(theme, 130, 40);
-            ScreenBuffer.Initialize(field.Width, field.Height, Theme.contrast, Theme.backGround);
-            var player = new Fighters.ThunderFighterOne(field, new Point2D(10, 5));
-            var level = GameLevel.Easy;
-
-            Engine engine = new Engine(field, player, level);
+            ScreenBuffer.Initialize(field.Width, field.Height, Theme.Contrast, Theme.BackGround);
+            
+            Engine engine = new Engine(field);
             engine.Start();
         }
     }
