4f5c32a687b3b4ea789402590ddd5cf8b0e1fa68
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2016-07-12 03:49:29 +0300

    Added Custom Exception Class

---
+++ ThunderFighter/NameFormatException.cs
@@ -0,0 +1,24 @@
+﻿namespace ThunderFighter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class NameFormatException : ArgumentException
+    {
+        public const int MinNameLength = 3;
+        public const int MaxNameLength = 10;
+
+        public NameFormatException(string message, string name)
+        {
+        }
+
+        public NameFormatException(string name) : this(string.Format("Name must be between {0} and {1} symbols long!", MinNameLength, MaxNameLength), name)
+        {
+        }
+
+        public NameFormatException(string message, string name, Exception innerException) : base(message, name, innerException)
+        {
+        }
+    }
+}
